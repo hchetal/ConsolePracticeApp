6b64ce309b31f6eb10c2289c93fe784445d24b9a
Himanshu Chetal <himanshu.chetal@outlook.com>
2018-07-07 23:55:13 +0800

    Adding more methods

--- ConsolePracticeApp/Program.cs
+++ ConsolePracticeApp/Program.cs
@@ -14,7 +14,58 @@ namespace ConsolePracticeApp
             DefaultDeclarations();
             NewingDataTypes();
             DataTypeFunctionality();
+            CharFunctionality();
+            ParseFromStrings();
+            ParseFromStringsWithTryParse();
             Console.ReadLine();
+        }
+
+        static void ParseFromStringsWithTryParse()
+        {
+            Console.WriteLine("=> Data type parsing with TryParse:");
+            if (bool.TryParse("True", out bool b))
+            {
+                Console.WriteLine("Value of b: {0}", b);
+            }
+            string value = "Hello";
+            if (double.TryParse(value, out double d))
+            {
+                Console.WriteLine("Value of d: {0}", d);
+            }
+            else
+            {
+                Console.WriteLine("Failed to convert the input ({0}) to a double", value);
+            }
+            Console.WriteLine();
+        }
+
+        private static void ParseFromStrings()
+        {
+            Console.WriteLine("=> Data type parsing:");
+            bool b = bool.Parse("True");
+            Console.WriteLine("Value of b: {0}", b);
+            double d = double.Parse("99.884");
+            Console.WriteLine("Value of d: {0}", d);
+            int i = int.Parse("8");
+            Console.WriteLine("Value of i: {0}", i);
+            char c = Char.Parse("w");
+            Console.WriteLine("Value of c: {0}", c);
+            Console.WriteLine();
+        }
+
+        private static void CharFunctionality()
+        {
+            Console.WriteLine("=> char type Functionality:");
+            char myChar = 'a';
+            Console.WriteLine("char.IsDigit('a'): {0}", char.IsDigit(myChar));
+            Console.WriteLine("char.IsLetter('a'): {0}", char.IsLetter(myChar));
+            Console.WriteLine("char.IsWhiteSpace('Hello There', 5): {0}",
+                char.IsWhiteSpace("Hello There", 5));
+            Console.WriteLine("char.IsWhiteSpace('Hello There', 6): {0}",
+                char.IsWhiteSpace("Hello There", 6));
+            Console.WriteLine("char.IsPunctuation('?'): {0}",
+                char.IsPunctuation('?'));
+            Console.WriteLine();
         }
 
         private static void DataTypeFunctionality()
